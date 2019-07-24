491a871324059d22ecfd847511b765cdc10d453b
zhangquanzhi <zhangquanzhi@jd.com>
2019-07-24 14:47:58 +0800

    example fix

--- examples/output_to_file/main.go
+++ examples/output_to_file/main.go
@@ -4,20 +4,29 @@ import (
     "fmt"
     "github.com/ssor/zlog"
     "gopkg.in/natefinch/lumberjack.v2"
+    "io"
     "time"
 )
 
 func main() {
 
-    name := "main"
-    logger := zlog.New()
-    logger.SetOutput(&lumberjack.Logger{
-        Filename:   name + ".log",
+    logFile := &lumberjack.Logger{
+        Filename:   "main.log",
         MaxSize:    1, // megabytes
         MaxBackups: 3,
         MaxAge:     28,    //days
         Compress:   false, // disabled by default
-    })
+    }
+
+    for _, name := range []string{"module1", "module2", "module3"} {
+        go startLogger(name, logFile)
+    }
+    select {}
+}
+
+func startLogger(name string, writer io.Writer) {
+    logger := zlog.New(name)
+    logger.SetOutput(writer)
 
     start := time.Now()
     duration := 5 * time.Minute
@@ -30,7 +39,7 @@ func main() {
         count++
         time.Sleep(1 * time.Millisecond)
         if count%1000 == 0 {
-            fmt.Println("now is ", now)
+            fmt.Println(name, " -> now is ", now)
         }
         logger.Infof("now is %s", now.String())
     }
